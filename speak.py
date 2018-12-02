118b9f8ef61bd859efc438b29fff3009b0daa5d8
Aaron <afink.2020@saintannsny.org>
2018-12-02 11:50:22 -0500

    Added stepping stones to help with reversing

--- speak.py
+++ speak.py
@@ -8,25 +8,31 @@ name = "John Jacob Jingleheimer Schmidt"
 # 2. Use print and a built-in method to print out the string "JOHN JACOB JINGELHEIMER SCHMIDT"
 
 
-# 3. Use print and a built-in method to print out the string "tdimhcS remiehelgniJ bocaJ nhoJ" (which is the name backwards)
+# 3. Use print and a built-in method to find out how many characters are in Mr. Schmidt's name.
 
 
-# 4. Use print and a built-in method to find out how many characters are in Mr. Schmidt's name.
+# 4. Use print and a built-in method to count how many times the letter i is in Mr. Schmidt's name.
 
 
-# 5. Use print and a built-in method to count how many times the letter i is in Mr. Schmidt's name.
+# 5. Use print and a built-in method to print out Mr. Schmidt's name, but with all the instances of the letter i removed or deleted.
 
 
-# 6. Use print and a built-in method to print out Mr. Schmidt's name, but with all the instances of the letter i removed or deleted.
+# 6. Use a built-in method to replace every letter J with the letter G instead. Bonus point if you can say the result out loud without laughing.
 
 
-# 7. Use a built-in method to replace every letter J with the letter G instead. Bonus point if you can say the result out loud without laughing.
+# 7. Chain a few methods together to print out the string with all the vowels removed. 
 
 
-# 8. Chain two methods together to print out the string "TDIMHCS REMIEHELGNIJ BOCAJ NHOJ", (which is the name in uppercase and backwards).
+# 8. Try to print the name as four separate strings by using a method that will split it up like this: ["John", "Jacob", "Jingleheimer", "Schmidt"]
 
 
-# 9. Chain a few methods together to print out the string with all the vowels removed. 
+# 9. Print out only the first four letters of the string.
 
 
-# 10. Try to print the name as four separate strings by using a method that will split it up like this: ["John", "Jacob", "Jingleheimer", "Schmidt"]
+# 10. Print out only the first ten letters of the string but make them all uppercase.
+
+
+# 11. Try to print out the string "tdimhcS remiehelgniJ bocaJ nhoJ" (which is the name backwards)
+
+
+# 12. Chain two methods together to print out the string "TDIMHCS REMIEHELGNIJ BOCAJ NHOJ", (which is the name in uppercase and backwards).
